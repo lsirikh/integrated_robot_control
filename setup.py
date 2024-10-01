8979f0bf9bef411e3760c05507e052bd9fb13fb7
lsirikh <lsirikh@naver.com>
2024-10-02 01:48:41 +0900

    version modified

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'integrated_robot_control'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.2',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
